5d0df2045c6bb7952e04eb5271811ffecb7e5074
shiva <Shiva Kumar>
2026-07-18 14:33:03 +0530

    Stage-1 Done

---
+++ mynitcbase/main.cpp
@@ -0,0 +1,26 @@
+#include "Buffer/StaticBuffer.h"
+#include "Cache/OpenRelTable.h"
+#include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  /* Initialize the Run Copy of Disk */
+  Disk disk_run;
+  // StaticBuffer buffer;
+  // OpenRelTable cache;
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, 7000);
+  char message[] = "hello\n";
+  memcpy(buffer + 20, message, 7);
+  Disk::writeBlock(buffer, 7000);
+
+  unsigned char buffer2[BLOCK_SIZE];
+  char message2[7];
+  Disk::readBlock(buffer2, 7000);
+  memcpy(message2, buffer2 + 20, 7);
+  std::cout << message2;
+
+  return 0;
+  // return FrontendInterface::handleFrontend(argc, argv);
+}
