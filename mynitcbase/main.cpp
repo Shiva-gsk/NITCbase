776ea088dce2b50b67c9da095bd3087a7f40d9a4
shiva <Shiva Kumar>
2026-07-22 21:15:51 +0530

    Stage-2 Ex-1

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -9,15 +9,12 @@ int main(int argc, char *argv[]) {
 
   // create objects for the relation catalog and attribute catalog
   RecBuffer relCatBuffer(RELCAT_BLOCK);
-  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
   HeadInfo relCatHeader;
-  HeadInfo attrCatHeader;
 
   // load the headers of both the blocks into relCatHeader and attrCatHeader.
   // (we will implement these functions later)
   relCatBuffer.getHeader(&relCatHeader);
-  attrCatBuffer.getHeader(&attrCatHeader);
 
 
   for (int i = 0; i < relCatHeader.numEntries; i++) {
@@ -28,16 +25,27 @@ int main(int argc, char *argv[]) {
 
     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
 
-    for (int j = 0; j < attrCatHeader.numEntries; j++) {
+    int attrBlockNum = ATTRCAT_BLOCK;
+    while (attrBlockNum != -1) {
+      RecBuffer attrCatBuffer(attrBlockNum);
+      HeadInfo attrCatHeader;
+      attrCatBuffer.getHeader(&attrCatHeader);
 
-      // declare attrCatRecord and load the attribute catalog entry into it
-      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-      attrCatBuffer.getRecord(attrCatRecord, j);
+      int nextAttrBlock = attrCatHeader.rblock;
 
-      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
-        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      for (int j = 0; j < attrCatHeader.numEntries; j++) {
+
+        // declare attrCatRecord and load the attribute catalog entry into it
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord, j);
+
+        if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
+          const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+          printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+        }
       }
+
+      attrBlockNum = nextAttrBlock;
     }
     printf("\n");
   }
